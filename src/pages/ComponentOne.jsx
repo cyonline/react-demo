62e972dab37120c82b4291a30affd902fae828c0
cyonline <chengyuonline@163.com>
2023-03-27 23:45:49 +0800

    update

--- src/pages/ComponentOne.jsx
+++ src/pages/ComponentOne.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 export default function ComponentOne (){
+    const [name,setName] = useState('')
+
+    const nameChange = (e) => {
+        console.info(e.target.value)
+        setName(e.target.value)
+    }
     return (
-        <div>1</div>
+        <div>
+            <input type="text" value={name} onChange={nameChange}/>
+            姓名:{name}
+        </div>
     )
 }
 
